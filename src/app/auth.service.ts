7e5693c8e570617dde0e032b70263110770a302a
Sirhit0 <118976176+Sirhit0@users.noreply.github.com>
2024-05-23 17:30:53 +0200

    1.

---
+++ src/app/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'http://127.0.0.1:5502'; // Cambia esto a la URL de tu backend
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, name: string, password: string): Observable<any> {
+    const loginData = { email, name, password };
+    return this.http.post(`${this.baseUrl}/auth/login`, loginData);
+  }
+}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service'; // Asegúrate de que la ruta es correcta
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService  // Inyecta el servicio aquí
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, name, password } = this.loginForm.value;
+      this.authService.login(email, name, password).subscribe(
+        (response: any) => {
+          // Manejar la respuesta del backend, por ejemplo, redirigir al usuario
+          this.router.navigate(['/ruta-destino']);
+        },
+        (error: any) => {
+          // Manejar errores
+          console.error(error);
+        }
+      );
+    }
+  }
 }
